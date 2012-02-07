8c07293508f95191a34b2a82810dc1dbdcec4b53
Brian Kurlychek <brianpedalsteel@brian-kurlychek-1.wireless.usm.maine.edu>
2012-02-07 14:17:45 -0500

    Final assignment submission

--- test/integration/books/BooksServiceIntegrationTests.groovy
+++ test/integration/books/BooksServiceIntegrationTests.groovy
@@ -3,11 +3,12 @@ package books
 import grails.test.*
 import books.*
 import org.junit.*
+import static.org.junit.Assert.*
 
 class BooksServiceIntegrationTests extends GrailsUnitTestCase{
     
     def booksService
-    
+    @Test
     void testStaticBooks() {
         
 		def staticBooks = booksService.getStaticBooks()
@@ -15,18 +16,18 @@ class BooksServiceIntegrationTests extends GrailsUnitTestCase{
 		assertEquals("Eric Carle", staticBooks.author)
                 assertEquals("The Hungry Caterpillar", staticBooks.title)
     }
-    
+    @Test
     void tryAnotherTest(){
         
         def testBook = booksService.getRandomBooks()
         assertTrue testBook != null
         
     }
-    
+    @Test
     void tryAThirdTest(){
         def book = new Books(title: "testTitle", author: "testAuthor", description: "testDescription", ISBN: "900-123456789", minimumGradeLevel: "testMin", maximumGradeLevel: "testMax",language: "English",category: "Reading Skills")
         book.save()
-        assertEquals 1,Books.list().size()
+        assertEquals 1, Books.list().size()
     }
   
 }
